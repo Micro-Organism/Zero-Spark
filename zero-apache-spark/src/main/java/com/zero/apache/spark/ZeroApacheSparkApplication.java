06567ed08b044102d67642af839ce9e9dec0c840
Micro-Organism <coolrabbit1993@gmail.com>
2024-12-09 20:06:28 +0800

    feat: springboot 集成 spark

---
+++ zero-apache-spark/src/main/java/com/zero/apache/spark/ZeroApacheSparkApplication.java
@@ -0,0 +1,13 @@
+package com.zero.apache.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroApacheSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroApacheSparkApplication.class, args);
+    }
+
+}
---
+++ zero-apache-spark/src/test/java/com/zero/apache/spark/ZeroApacheSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.apache.spark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApacheSparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
